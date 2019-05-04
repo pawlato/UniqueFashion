2cd64fb93400a238d3b23fce420ed380f7b229bb
Marek <pawlat.olga@gmail.com>
2019-05-04 22:06:10 +0200

    slider & menu

--- js/main.js
+++ js/main.js
@@ -7,16 +7,13 @@ $(document).ready(function () {
     scrollSlow();
     showSections();
     showText();
-    parallax();
-    showIcons();
     sliderReady();
 });
 
 $(window).scroll(function () {
     showLogo();
     showSections();
-    showText()
-    showIcons();
+    showText();
     hideMenu();
 });
 
@@ -165,6 +162,8 @@ function showSections() {
     var position3 = header3.position();
     var header4 = $(".header4");
     var position4 = header4.position();
+    var header4 = $(".header4");
+    var position4 = header4.position();
     var input1 = $(".input1");
     var input1_position = input1.offset();    
     var input2 = $(".input2");
@@ -233,87 +232,4 @@ function showText() {
         text.removeClass('show-text');
     }
 
-};
-
-function parallax() {
-    window.addEventListener('scroll', parallax, false);
-    var backgroundTop = $('.logo-back');
-    backgroundTop.css({
-        bottom: -(window.pageYOffset / 3) + "px"
-    });
-
-};
-
-function showIcons() {
-    var icons = $('.code-programs');
-    var position = icons.position();
-    if ($(window).scrollTop() >= position.top - 400) {
-        icons.each(function (index) {
-            var element = $(this);
-            setTimeout(function () {
-                element.addClass("show-icons");
-            }, index == 0 ? 0 : 500 * index);
-        })
-    }
-};
-
-function sliderReady() {
-
-var slider = $('#slider');
-
-var slideShow = $('.slide-show'); 
-
-var slideCount = slideShow.children().length;
-
-var slideWidth = 100/slideCount;
-
-var slideIndex = 0;
-
-    
-//        Ustawianie szerokosci kontenera
-    slideShow.css('width' , slideCount* 100 + '%');
-    
-//        Ustawianie marginesow i szerokosci pojedynczych slideow
-    slideShow.find('.single-slide').each(function(index) {
-        var leftPercent = (slideWidth * index) + '%';
-        $(this).css('margin-left', leftPercent);
-        $(this).css('width', slideWidth + '%');
-    });
-    
-//        Wywolanie funkcji slide na click
-    $('.prev-slide').click(function(event) {
-        event.preventDefault();
-        slide(slideIndex -1);
-    });
-    
-    $('.next-slide').click(function(event) {
-        event.preventDefault();
-        slide(slideIndex +1);
-    });
-    
-//        Definicja funkcji Slide
-    function slide(newSlideIndex) {
-//        Sprawdzamy czy pierwszy i ostatni zeby nie robil nic
-        if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
-            return;
-        }
-        
-//        elementy slide caption i przypisanie do zmiennej
-        var slideCaption = slider.find('.slider-caption').eq(newSlideIndex);
-        
-        
-//        Zmienna trzymajaca margines lewy do przesuwania kontenera
-        var marginLeft = ((newSlideIndex * (-100)) + '%');
-        
-//        Animacja na slideshow
-        slideShow.animate({'margin-left': marginLeft}, 800, function() {
-//            Przypisz do slideIndex nowy index Slidu
-            slideIndex = newSlideIndex;
-            
-//            pokaz napis przez fadeIn
-            slideCaption.fadeIn('slow');
-        });
-        
-    };
-    
 };
